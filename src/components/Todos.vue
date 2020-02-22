93a325d8025a76e2e7cf7d7b2ca2783a52e9cdfe
HironoriSato <maza.hs5360@gmail.com>
2020-02-22 14:52:59 +0900

    [add]Todos画面を作成

--- src/components/Todos.vue
+++ src/components/Todos.vue
@@ -1,11 +1,35 @@
 <template>
   <div>
     <h3>Todos</h3>
+    <div class="todos">
+      <div class="todo" v-for="todo in allTodos" :key="todo.id">{{ todo.title }}</div>
+    </div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from "vuex";
+
 export default {
-    name: 'Todos'
+  name: "Todos",
+  computed: mapGetters(["allTodos"])
 };
 </script>
+
+<style scoped>
+	.todos {
+		display: grid;
+		grid-template-columns: repeat(3, 1fr);
+		grid-gap: 1rem;
+	}
+
+	.todo {
+		border: 1px solid white;
+		background: lightblue;
+		border-radius: 5px;
+		padding: 1rem;
+		text-align: center;
+		position: relative;
+		cursor: pointer;
+	}
+</style>
