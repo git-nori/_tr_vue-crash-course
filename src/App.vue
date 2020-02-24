32dd8e274b67a0ab33d55ba6cf4ab6514efee456
HironoriSato <maza.hs5360@gmail.com>
2020-02-24 09:31:50 +0900

    [add]AddTodo画面を作成

--- src/App.vue
+++ src/App.vue
@@ -2,17 +2,20 @@
   <div id="app">
     <div class="container">
       <Todos />
+      <AddTodo />
     </div>
   </div>
 </template>
 
 <script>
 import Todos from "./components/Todos.vue";
+import AddTodo from "./components/AddTodo.vue"
 
 export default {
   name: "App",
   components: {
     Todos,
+    AddTodo
   }
 };
 </script>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,52 @@
+<template>
+	<div>
+		<h3>Add Todo</h3>
+		<div class="add">
+			<form @submit="onSubmit">
+				<input type="text" v-model="title" placeholder="Add Todo...">
+				<input type="submit" value="Submit">
+			</form>
+		</div>
+	</div>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+
+export default {
+		name: 'AddTodo',
+		data() {
+			return {
+				title: ''
+			}
+		},
+		methods: {
+			...mapActions(['addTodo']),
+			onSubmit(e){
+				e.preventDefault();
+				this.addTodo(this.title)
+			}
+		},
+}
+</script>
+
+<style scoped>
+	form {
+		display: flex;
+	}
+
+	input[type="text"]{
+		flex: 10;
+		padding: 10px;
+		border: 1px solid lightblue;
+		outline: 0px;
+	}
+
+	input[type="submit"]{
+		flex: 2;
+		background: lightblue;
+		color: white;
+		border: 1px solid lightblue;
+		cursor: pointer;
+	}
+</style>
